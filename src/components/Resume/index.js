b67f79e15bf842e36fc3e827d90cd97760a64a47
Merrick Watts <merricklwatts@gmail.com>
2022-08-25 10:59:28 -0600

    adding some final touches

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,24 +2,12 @@ import React from "react";
 
 function Resume() {
   return (
-    <div className="containerBackdrop resume-holder">
-      <h2 className="px-2 py-2">My Skills</h2>
-      <ul>
-        <li className="my-1">JavaScrpt</li>
-        <li className="my-1">CSS</li>
-        <li className="my-1">HTML</li>
-        <li className="my-1">React</li>
-        <li className="my-1">MySQL</li>
-        <li className="my-1">Express</li>
-        <li className="my-1">Object Oriented Programing</li>
-        <li className="my-1">NoSQL Databases</li>
-      </ul>
-
-      <h2 className="px-2 py-2">A little more about me:</h2>
-      <p className="px-2 py-2">
-        I am self motivated and a hard worker, I love to confront problems that
-        I might not know all the answers to, it gives me a good chance to learn!
-      </p>
+    <div className="resume-holder">
+      <iframe
+        className="resume"
+        scrolling="no"
+        src="https://docs.google.com/document/d/e/2PACX-1vQ4HH6CVaOO7XuE306W_OKsH36MEV3Iv26t4B1wuvIE6hwbFKl4UCCtpbsIOWTnCg/pub?embedded=true"
+      ></iframe>
     </div>
   );
 }
